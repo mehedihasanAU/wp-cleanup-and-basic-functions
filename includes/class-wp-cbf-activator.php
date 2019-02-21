6da2df4df07f11baaab22423ffc36e74ab8f74d8
A B M Mehedi HASAN <mehedi.aiub@gmail.com>
2019-02-21 11:49:59 +1100

    Add files via upload

---
+++ includes/class-wp-cbf-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://mehedihasan.me
+ * @since      1.0.0
+ *
+ * @package    Wp_Cbf
+ * @subpackage Wp_Cbf/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Cbf
+ * @subpackage Wp_Cbf/includes
+ * @author     A B M Mehedi Hasan <mehedi.aiub@gmail.com>
+ */
+class Wp_Cbf_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-wp-cbf-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://mehedihasan.me
+ * @since      1.0.0
+ *
+ * @package    Wp_Cbf
+ * @subpackage Wp_Cbf/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Cbf
+ * @subpackage Wp_Cbf/includes
+ * @author     A B M Mehedi Hasan <mehedi.aiub@gmail.com>
+ */
+class Wp_Cbf_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
